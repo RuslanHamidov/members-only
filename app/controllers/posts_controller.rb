837fae8af41e4b7c9a9830e4c7e016f29568611b
Ruslan Hamidov <ruslanhamidov@Ruslans-MBP.localdomain>
2024-10-06 21:47:31 +0400

    add post, user devise, have bug for post submit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,30 @@
 class PostsController < ApplicationController
 	before_action :authenticate_user!, except: [:index]
 
+	def index
+		@posts = Post.all
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
 
 	def new
 		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(post_params)
+
+		if @post.save
+			
+		else
+			render :new, status: :unprocessable_entity
+		end
+	end
+
+	private
+	def post_params
+		params.require(:post).permit(:title, :body)
 	end
 end
